59e7bbd0372c14a6e8b64a3d426173b1baf55d69
Tiago Cardoso <tiago.card2121@gmail.com>
2025-03-27 02:10:42 -0300

    Atualização de bugs ajustado o salvamento das cores altomaticas, ajustando a apariçao do NAM e criado uma mensagem de notificação de termino do sorteio

--- index.js
+++ index.js
@@ -15,12 +15,20 @@ let totalPlayersCount = 0;
 let maxPlayersPerColor = 0;
 let colorCount = {};
 let shuffledColors = [];
+let isDrawing = true;
+
+document.addEventListener('DOMContentLoaded', initialize);
 
 function calculateSum() {
     const num1 = parseFloat(player.value);
     const num2 = parseFloat(numberOfPlayer.value);
     const sum = num1 / num2;
     result.textContent = 'Quantidade de times é: ' + sum;
+
+    if (isNaN(sum)) {
+        result.textContent = 'Quantidade de times é: 0';
+        return;
+    }
 
     localStorage.setItem('player', JSON.stringify(num1))
     localStorage.setItem('numberOfPlayer', JSON.stringify(num2))
@@ -35,6 +43,16 @@ if (player) {
 
 if (numberOfPlayer) {
     numberOfPlayer.addEventListener('input', calculateSum);
+}
+
+if (localStorage.getItem('player') && player) {
+    const playerValue = JSON.parse(localStorage.getItem('player'))
+    player.value = playerValue
+}
+
+if (localStorage.getItem('numberOfPlayer') && numberOfPlayer) {
+    const numberOfPlayerValue = JSON.parse(localStorage.getItem('numberOfPlayer'))
+    numberOfPlayer.value = numberOfPlayerValue
 }
 
 function createColorInputs(sum) {
@@ -52,14 +70,15 @@ function createColorInputs(sum) {
 }
 
 function storeColors() {
-    const colorInputs = document.querySelectorAll('input[type="color"]');
     const colorArray = [];
-
+    const colorInputs = document.querySelectorAll('input[type="color"]');
+    console.log(colorArray)
     colorInputs.forEach(input => {
         colorArray.push(input.value);
     });
-
+    
     localStorage.setItem('colors', JSON.stringify(colorArray));
+    loadColors();
 }
 
 function loadColors() {
@@ -75,35 +94,12 @@ function loadColors() {
                 input.value = color;
 
                 colorInputsDiv.appendChild(input);
-
-                input.addEventListener('input', storeColors);
+                input.addEventListener('change', storeColors);
             });
         }
     }
-
-    if (localStorage.getItem('player') && player) {
-        const playerValue = JSON.parse(localStorage.getItem('player'))
-        player.value = playerValue
-    }
-
-    if (localStorage.getItem('numberOfPlayer') && numberOfPlayer) {
-        const numberOfPlayerValue = JSON.parse(localStorage.getItem('numberOfPlayer'))
-        numberOfPlayer.value = numberOfPlayerValue
-    }
-
-    if (localStorage.getItem('player') && localStorage.getItem('numberOfPlayer')) {
-        const resultValue = JSON.parse(localStorage.getItem('result'));
-
-        if (result && resultValue) {
-            result.innerHTML = `Quantidade de times é: ${resultValue}`;
-        }
-    } else {
-        if (result) {
-            result.innerHTML = '';
-        }
-    }
 }
-// Função para exibir a cor aleatória
+
 function displayRandomColor() {
     const storedColors = JSON.parse(localStorage.getItem('colors'));
     const playersPerTeam = parseInt(localStorage.getItem('player'), 10);
@@ -121,16 +117,6 @@ function displayRandomColor() {
         shuffledColors = shuffleArray([...storedColors]);
     }
 
-    // Verifica se todos os jogadores já foram sorteados
-    if (totalPlayers >= totalPlayersCount) {
-        alert('Todos os jogadores foram sorteados!');
-        if (btnDrawTeam) {
-            btnDrawTeam.disabled = true;
-        }
-        return;
-    }
-
-    // Sorteia uma cor aleatória, mas garante que cada cor só pode ser sorteada até 'playersPerTeam' vezes
     let randomColor;
     do {
         randomColor = shuffledColors[Math.floor(Math.random() * shuffledColors.length)];
@@ -156,9 +142,9 @@ function displayRandomColor() {
     }
 
     if (totalPlayers >= totalPlayersCount) {
-        alert('Todos os jogadores foram sorteados!');
+        showNotification('Jogo finalizado')
         if (btnDrawTeam) {
-            btnDrawTeam.disabled = true
+            btnDrawTeam.disabled = true;
         }
     }
 }
@@ -171,9 +157,35 @@ function shuffleArray(array) {
     return array;
 }
 
+function showNotification(message) {
+    const notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.style.width = '100%';
+    notification.style.textAlign = 'center';
+    notification.style.position = 'fixed';
+    notification.style.bottom = '190px';
+    notification.style.left = '50%';
+    notification.style.transform = 'translateX(-50%)';
+    notification.style.padding = '15px 30px';
+    notification.style.backgroundColor = '#04022b';
+    notification.style.color = '#fff';
+    notification.style.fontWeight = 'bold';
+    notification.style.zIndex = '1000';
+    notification.style.opacity = '1';
+    notification.style.transition = 'opacity 1s';
+    notification.innerText = message;
+
+    document.body.appendChild(notification);
+
+    // Após 3 segundos, desaparece a notificação
+    setTimeout(() => {
+        notification.style.opacity = '0';
+        setTimeout(() => document.body.removeChild(notification), 1000); // Remove após a animação
+    }, 3000);
+}
+
 function initialize() {
     loadColors();
-
     document.querySelectorAll('input[type="color"]').forEach(input => {
         input.addEventListener('input', storeColors);
     });
@@ -185,7 +197,7 @@ function initialize() {
                 return
             }
             btnDrawTeam.style.animation = 'shake .5s infinite';
-            
+
             if (navigator.vibrate) {
                 navigator.vibrate(2000);
             }
@@ -214,7 +226,7 @@ function showModal() {
     modal.style.justifyContent = 'center';
     modal.style.alignItems = 'center';
     modal.style.gap = '15px';
-    modal.style.zIndex = '1000';
+    modal.style.zIndex = '999';
     modal.innerText = 'A cor do seu time e:'
     modal.style.fontWeight = 'bold'
 
@@ -246,12 +258,14 @@ function showModal() {
 
     document.body.appendChild(modal);
 
-    backButton.addEventListener('click', () => {
-        document.body.removeChild(modal);
-    });
+    if (backButton) {
+        backButton.addEventListener('click', () => {
+            document.body.removeChild(modal);
+        });
+    }
 }
 
-if(btnBack) {
+if (btnBack) {
     btnBack.addEventListener('click', () => {
         window.location.href = 'index.html';
     })
@@ -267,11 +281,8 @@ if (reset) {
     })
 }
 
-document.addEventListener('DOMContentLoaded', initialize);
-
-reload.addEventListener('click', () => {
-    location.reload()
-})
-
-
-
+if(btnReload) {
+    btnReload.addEventListener('click', () => {
+        location.reload()
+    })
+}
